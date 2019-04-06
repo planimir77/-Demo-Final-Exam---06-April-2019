defa8123dae7211ae39f5821ca1b52acf531aa78
planimir77 <34979738+planimir77@users.noreply.github.com>
2019-04-06 16:58:13 +0300

    Demo Exam 06.04.2019

---
+++ 01. Dictionary/Program.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _01._Dictionary
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var dict = new Dictionary<string, List<string>>();
+
+            string inputString = Console.ReadLine();
+
+            string[] arr = inputString.Split(" | ", StringSplitOptions.RemoveEmptyEntries).ToArray();
+            foreach (var item in arr)
+            {
+                string[] temp = item.Split(": ", StringSplitOptions.RemoveEmptyEntries).ToArray();
+                string key = temp[0];
+                string value = temp[1];
+                if (!dict.ContainsKey(key))
+                {
+                    dict.Add(key, new List<string>());
+                }
+                dict[key].Add(value);
+            }
+
+            string[] words = Console.ReadLine().Split(" | ",StringSplitOptions.RemoveEmptyEntries).ToArray();
+            string command = Console.ReadLine();
+
+            if (command=="List")
+            {
+                foreach (var kvp in dict.OrderBy(x=>x.Key))
+                {
+                    Console.Write(kvp.Key+" ");
+                }
+                Console.ReadLine();
+            }
+            else if (command == "End")
+            {
+                for (int i = 0; i < words.Length; i++)
+                {
+                    if (dict.ContainsKey(words[i]))
+                    {
+                        Console.WriteLine(words[i]);
+                        foreach (var kvp in dict)
+                        {
+                            if (kvp.Key== words[i])
+                            {
+                                foreach (var item in kvp.Value.OrderByDescending(x=>x.Count()))
+                                {
+                                    Console.WriteLine(" -"+item);
+                                }
+                            }
+                        }
+
+                    }
+                }
+                
+                
+            }
+            
+        }
+    }
+}
---
+++ 02. Deciphering/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+
+namespace _02._Deciphering
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string message = Console.ReadLine();
+
+            string[] inOut = Console.ReadLine()
+                             .Split(' ', StringSplitOptions.RemoveEmptyEntries)
+                             .ToArray();
+            string firstSing = inOut[0];
+            string secondSing = inOut[1];
+
+            Regex regex = new Regex(@"([d-z]|\||#|{|})");
+            MatchCollection match = regex.Matches(message);
+            StringBuilder sb = new StringBuilder();
+
+            foreach (Match item in match)
+            {
+                sb.Append((char)(Char.Parse(item.ToString())-3));
+            }
+
+            if (!(sb.ToString().Length ==  message.Length))
+            {
+                Console.WriteLine("This is not the book you are looking for.");
+                return;
+            }
+
+            sb = sb.Replace(firstSing,secondSing);
+            Console.WriteLine(sb);
+        }
+    }
+}
